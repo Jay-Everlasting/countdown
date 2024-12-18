48668041f347ea69b9ff9e7a8eb336756e82114e
Adel Lis <adel.lis.work@gmail.com>
2024-12-18 01:59:44 +0100

    Correct visual bugs

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict";
 
 // Set the target date in UTC
-const targetDate = new Date("2024-12-22T08:25:00Z").getTime(); // 10:25 AM Amsterdam time converted to UTC
-// const targetDate = new Date("2024-12-18T22:30:00Z").getTime(); // 10:25 AM Amsterdam time converted to UTC
+// const targetDate = new Date("2024-12-22T08:25:00Z").getTime(); // 10:25 AM Amsterdam time converted to UTC
+const targetDate = new Date("2024-12-16T22:30:00Z").getTime(); // 10:25 AM Amsterdam time converted to UTC
 // const meetingDate = new Date("December 23, 2024 20:45:00 GMT+10").getTime(); // 20:45 Bristbane time
-const meetingDate = new Date("December 18, 2024 14:55:00 GMT+10").getTime();
+const meetingDate = new Date("December 16, 2024 14:55:00 GMT+10").getTime();
 
 // Update the countdown every second
 const countdownInterval = setInterval(function () {
@@ -15,6 +15,7 @@ const countdownInterval = setInterval(function () {
   // 380352614
   if (timeLeft <= 0 && timeLeftMeet <= 0) {
     document.getElementById("container").innerHTML = "🧑🏼‍❤️‍💋‍👩🏽";
+    document.getElementById("container").classList.add("big-icon");
     clearTimeout(countdownInterval);
     return;
   }
